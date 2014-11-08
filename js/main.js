ec07a32926376006f149bc3e2ee288d4777342ba
Brandon Morrison <brandon@brandonmorrison.com>
2014-11-08 15:45:22 -0500

    Better start position

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   var colors = ['rgb(166,97,26)','rgb(223,194,125)','rgb(245,245,245)','rgb(128,205,193)','rgb(1,133,113)'];
 
   var map = L.map('map', {
-    center: [40.735806, -73.990429],
+    center: [40.735806, -73.989929],
     zoom: 18
   });
 
